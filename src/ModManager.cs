e1954b94ff39f0d1dfd11023b763052aad39c509
Bright1192 <2166085463@qq.com>
2023-03-23 11:22:27 +0800

    v1.3.0 raw

--- src/ModManager.cs
+++ src/ModManager.cs
@@ -1,9 +1,10 @@
 ﻿using Il2Cpp;
 using Il2CppInterop.Runtime.Injection;
 using Il2CppMainUI;
+using Il2CppSystem.Collections.Generic;
 using Il2CppTMPro;
 using System;
-using Il2CppSystem.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using UnityEngine;
 
@@ -80,6 +81,40 @@ namespace LimbusLocalize
             string NickName = Resources.Load<TextAsset>("Story/ScenarioModelCode").ToString();
             File.WriteAllText(LimbusLocalizeMod.path + "/Localize/NickName.json", NickName);
 
+        }
+        public static void MirrorDungeonGiveToString()
+        {
+            string MirrorDungeonGive = "Bright#1192 制作";
+            MirrorDungeonStaticData dungeon = Singleton<StaticDataManager>.Instance.MirrorDungeonInfoList.GetDungeon(1);
+            for (int i = 0; i < dungeon.floors.Count; i++)
+            {
+                var floor = dungeon.floors[i];
+                for (int i2 = 0; i2 < floor.MirrorSector.Count; i2++)
+                {
+                    var Sector = floor.MirrorSector[i2];
+                    var Reward = Sector.RewardList[^1];
+                    MirrorDungeonGive += string.Format("\nFloor{0} Sector{1}:\n   加成:{2}\n   非加:{3}", i, i2, RewardToString(Reward.ChanceReward), RewardToString(Reward.NormalReward));
+
+                }
+            }
+            File.WriteAllText(LimbusLocalizeMod.path + "/镜牢进度对应奖励一览.txt", MirrorDungeonGive);
+        }
+        public static string RewardToString(MirrorDungeonSectorRewardBaseData rewardBaseData)
+        {
+            int PASS_POINT = 0;
+            string ITEM = "";
+            foreach (var element in rewardBaseData.RewardList)
+            {
+                if (element.Type == ELEMENT_TYPE.BATTLEPASS_POINT)
+                {
+                    PASS_POINT += element.Num;
+                }
+                else if (element.Type == ELEMENT_TYPE.ITEM)
+                {
+                    ITEM += string.Format(" {0} {1}个", Singleton<StaticDataManager>.Instance.ItemList.GetData(element.Id).GetItemName(), element.Num);
+                }
+            }
+            return string.Format("经验{0} 点数{1} 随机{2}自选{3} 物品{4}", rewardBaseData.acquireUserExp, PASS_POINT, rewardBaseData.randomPackagePiece, rewardBaseData.optionalPackagePiece, ITEM);
         }
     }
 }
