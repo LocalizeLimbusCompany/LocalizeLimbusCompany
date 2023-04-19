b5cab4e3b056d8eec7896cada2c130eb2e886615
Bright1192 <2166085463@qq.com>
2023-04-19 11:35:29 +0800

    删除 github-page 更改github文件夹 修改build命令

--- build.ps1
+++ build.ps1
@@ -19,37 +19,28 @@ Remove-Item $Path/Microsoft.Extensions.Logging.Abstractions.dll
 New-Item -Path "$Path" -Name "LimbusLocalize" -ItemType "directory" -Force
 New-Item -Path "$Path/LimbusLocalize" -Name "Mods" -ItemType "directory" -Force
 New-Item -Path "$Path/LimbusLocalize/Mods" -Name "Localize" -ItemType "directory" -Force
-Copy-Item -Path assets/Localize/CN $Path/LimbusLocalize/Mods/Localize -Force -Recurse
-Copy-Item -Path assets/Localize/Readme $Path/LimbusLocalize/Mods/Localize -Force -Recurse
+Copy-Item -Path Localize/CN $Path/LimbusLocalize/Mods/Localize -Force -Recurse-Recurse
 Copy-Item -Path $Path/LimbusLocalize.dll -Destination $Path/LimbusLocalize/Mods -Force
 7z a -t7z "$Path/LimbusLocalize_$version.7z" "./$Path/LimbusLocalize/*" -mx=9 -ms
 $tag=$(git describe --tags --abbrev=0)
-$changedFiles=$(git diff --name-only HEAD $tag -- assets/Localize/CN/)
-$changedFiles2=$(git diff --name-only HEAD $tag -- assets/Localize/Readme/)
+$changedFiles=$(git diff --name-only HEAD $tag -- Localize/CN/)
 # OTA
 New-Item -Path "$Path" -Name "LimbusLocalize_OTA" -ItemType "directory" -Force
 New-Item -Path "$Path/LimbusLocalize_OTA" -Name "Mods" -ItemType "directory" -Force
 Copy-Item -Path $Path/LimbusLocalize.dll -Destination $Path/LimbusLocalize_OTA/Mods -Force
 New-Item -Path "$Path/LimbusLocalize_OTA/Mods" -Name "Localize" -ItemType "directory" -Force
-New-Item -Path "$Path/LimbusLocalize_OTA/Mods/Localize" -Name "Readme" -ItemType "directory" -Force
 New-Item -Path "$Path/LimbusLocalize_OTA/Mods/Localize" -Name "CN" -ItemType "directory" -Force
 # Copy the changed files to the release directory
 $changedFilesList = $changedFiles -split " "
 foreach ($file in $changedFilesList) {
     if (Test-Path -Path $file) {
         $destination = "$Path/LimbusLocalize_OTA/Mods/Localize/CN/$file"
-        $destination = $destination.Replace("assets/Localize/CN/", "")
+        $destination = $destination.Replace("Localize/CN/", "")
         $destinationDirectory = Split-Path -Path $destination -Parent
         if (!(Test-Path -Path $destinationDirectory)) {
             New-Item -ItemType Directory -Force -Path $destinationDirectory
         }
         Copy-Item -Path $file -Destination $destination -Force -Recurse
-    }
-}
-$changedFilesList2 = $changedFiles2 -split " "
-foreach ($file2 in $changedFilesList2) {
-	if(Test-Path $file2){
-		Copy-Item -Path $file2 $Path/LimbusLocalize_OTA/Mods/Localize/Readme -Force
     }
 }
 7z a -t7z "$Path/LimbusLocalize_OTA_$version.7z" "./$Path/LimbusLocalize_OTA/*" -mx=9 -ms
