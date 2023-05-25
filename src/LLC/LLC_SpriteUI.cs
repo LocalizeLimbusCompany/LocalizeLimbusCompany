6ce066bf80fd8c43cc7faa54b217f9965273d7a5
Bright1192 <2166085463@qq.com>
2023-05-25 08:06:06 +0800

    LLC_Combo

---
+++ src/LLC/LLC_SpriteUI.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Il2CppBattleUI;
+
+namespace LimbusLocalize
+{
+    public static class LLC_SpriteUI
+    {
+        [HarmonyPatch(typeof(ParryingTypoUI), nameof(ParryingTypoUI.Init))]
+        [HarmonyPostfix]
+        private static void ParryingTypoUI_Init(ParryingTypoUI __instance)
+        {
+            __instance.img_parryingTypo.sprite = LLC_ReadmeManager.ReadmeSprites["LLC_Combo"];
+        }
+    }
+}
