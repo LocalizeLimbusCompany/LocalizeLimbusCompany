f7fefa6d3ecf8f46b8ffe4ff635c69e2c7fd3d90
Bright1192 <87061166+Bright1192@users.noreply.github.com>
2023-07-29 09:12:52 +0800

    RandomAllLoadCG

--- src/LLC/LLC_LoadingManager.cs
+++ src/LLC/LLC_LoadingManager.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using Il2Cpp;
+using MelonLoader;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,6 +13,9 @@ namespace LimbusLocalize
         static List<string> LoadingTexts = new();
         static string Touhou;
         static readonly string Raw = "<bounce f=0.5>NOW LOADING...</bounce>";
+        public static MelonPreferences_Entry<bool> RandomAllLoadCG = LCB_LLCMod.LLC_Settings.CreateEntry("RandomAllLoadCG", true, null, "是否从所有已到达进度中随机选择载入CG ( true | false )");
+        public static MelonPreferences_Entry<bool> RandomLoadText = LCB_LLCMod.LLC_Settings.CreateEntry("RandomLoadText", true, null, "是否随机选择载入标语,即右下角的[NOW LOADING...] ( true | false )");
+        public static int ArchiveCGId;
         static LLC_LoadingManager()
         {
             InitLoadingTexts();
@@ -33,23 +37,69 @@ namespace LimbusLocalize
             {
                 return default;
             }
-            return list[Random.Range(0, list.Count - 1)];
+            return list[Random.Range(0, list.Count)];
         }
         [HarmonyPatch(typeof(LoadingSceneManager), nameof(LoadingSceneManager.Start))]
         [HarmonyPostfix]
         private static void LSM_Start(LoadingSceneManager __instance)
         {
+            if (!RandomLoadText.Value)
+                return;
             var loadingText = __instance._loadingText;
             loadingText.font = LCB_Chinese_Font.tmpchinesefonts[0];
             loadingText.fontMaterial = LCB_Chinese_Font.tmpchinesefonts[0].material;
             loadingText.fontSize = 40;
-            int random = Random.Range(0, 100);
+            int random = Random.Range(0, 101);
             if (random < 25)
                 loadingText.text = Raw;
             else if (random < 50)
                 loadingText.text = Touhou;
             else
                 loadingText.text = SelectOne(LoadingTexts);
+        }
+        [HarmonyPatch(typeof(LoadingCGDataList), nameof(LoadingCGDataList.GetDataByStageNode))]
+        [HarmonyPrefix]
+        private static void RandomAllLoadCG_GetDataByStageNode(LoadingCGDataList __instance, ref int id, ref int SubChapterId)
+        {
+            if (!RandomAllLoadCG.Value)
+                return;
+            if (ArchiveCGId != id)
+            {
+                ArchiveCGId = id;
+                __instance._loadingCGDataDic_Stage[1192] = AllLoadingCGData(__instance._loadingCGDataDic_Stage, id);
+            }
+            id = SubChapterId = 1192;
+        }
+        [HarmonyPatch(typeof(LoadingCGDataList), nameof(LoadingCGDataList.GetDataByDungeonNode))]
+        [HarmonyPrefix]
+        private static void RandomAllLoadCG_GetDataByDungeonNode(LoadingCGDataList __instance, ref int id, ref int SubChapterId)
+        {
+            if (!RandomAllLoadCG.Value)
+                return;
+            if (ArchiveCGId != id)
+            {
+                ArchiveCGId = id;
+                __instance._loadingCGDataDic_Dungeon[1192] = AllLoadingCGData(__instance._loadingCGDataDic_Dungeon, id);
+            }
+            id = SubChapterId = 1192;
+        }
+        private static Il2CppSystem.Collections.Generic.List<LoadingCGData> AllLoadingCGData(Il2CppSystem.Collections.Generic.Dictionary<int, Il2CppSystem.Collections.Generic.List<LoadingCGData>> dic, int id)
+        {
+            LoadingCGData allloadingCGData = new()
+            {
+                id = 1192
+            };
+            Il2CppSystem.Collections.Generic.List<LoadingCGData> allloadingCGDatas = new();
+            allloadingCGDatas.Add(allloadingCGData);
+
+            int idx = dic.FindEntry(id / 100) + 1;
+
+            for (int i = 0; i < idx; i++)
+                foreach (var items in dic._entries[i].value)
+                    if (items.id != 1192 && items.id <= id)
+                        foreach (var item in items.clearCGList)
+                            allloadingCGData.clearCGList.Add(item);
+            return allloadingCGDatas;
         }
     }
 }
