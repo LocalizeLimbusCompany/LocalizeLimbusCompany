bda32644504d8686620d0124572bd516247e82f3
N071 <128023427+N071@users.noreply.github.com>
2024-06-15 21:14:37 +0800

    06-15 ParaToGitWork & v0.6.39

--- src/LCB_LLCMod.cs
+++ src/LCB_LLCMod.cs
@@ -17,7 +17,7 @@ namespace LimbusLocalize
         public static string GamePath;
         public const string GUID = "Com.Bright.LocalizeLimbusCompany";
         public const string NAME = "LimbusLocalizeMod";
-        public const string VERSION = "0.6.38";
+        public const string VERSION = "0.6.39";
         public const string AUTHOR = "Bright";
         public const string LLCLink = "https://github.com/LocalizeLimbusCompany/LocalizeLimbusCompany";
         public static Action<string, Action> LogFatalError { get; set; }
